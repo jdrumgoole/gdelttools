3ac006f49aa89e4cad92c7656bdace5112642199
Joe Drumgoole <Joe.Drumgoole@mongodb.com>
2022-04-26 08:18:34 +0100

    Added mongoimport.py class
    Added a new class wrapper to support integrating mongoimport into the gdeltloader.

---
+++ gdelttools/mongoimport.py
@@ -0,0 +1,70 @@
+import os
+from enum import Enum
+import json
+import glob
+
+
+class FileType(Enum):
+    tsv = "tsv"
+    csv = "csv"
+
+    def __str__(self):
+        return self.value
+
+
+class InsertMode(Enum):
+    upsert = "upsert"
+
+    def __str__(self):
+        return self.value
+
+
+
+
+class MongoImport:
+    '''
+    Implement this script for mongoimport. We expect mongoimport to be installed
+    and be available on the path.
+
+    mongoimport --db=GDELT2 --collection=eventscsv  --type=tsv --fieldFile=gdelt_field_file.ff --mode=upsert --writeConcern "{w:1}" --columnsHaveTypes --file=$i $*
+    '''
+
+    def __init__(self,
+                 database_name: str="GDELT2",
+                 collection_name: str="eventscsv",
+                 file_type : FileType = FileType.tsv,
+                 field_file: str = "gdelt_field_file.ff",
+                 insert_mode : InsertMode = InsertMode.upsert,
+                 write_concern : str = "{w:1}"):
+        """
+        Define init parameters for mongoimport command
+        :param database: The mongodb database name we will be importing to
+        :param collection: The collection name we will be importing to
+        """
+
+        self._database_name = database_name
+        self._collection_name = collection_name
+        self._file_type = file_type
+        self._field_file = field_file
+        self._insert_mode = insert_mode
+        self._write_concern = write_concern
+
+    def command(self, input_file:str):
+
+        return f"mongoimport --db={self._database_name} --collection={self._collection_name} " +\
+               f"--type={self._file_type.value} --fieldFile={self._field_file} " +\
+               f"--mode={self._insert_mode.value} --writeConcern '{self._write_concern}' --columnsHaveTypes " +\
+               f"--file={input_file}"
+
+    def import_data(self, arg:str):
+        args = glob.glob(arg)
+        for i in args:
+            print(f"Running in: {os.getcwd()}")
+            print(self.command(i))
+            os.system(f"{self.command(i)}")
+
+
+if __name__ == "__main__":
+    m = MongoImport()
+    m.import_data("*.export.CSV")
+
