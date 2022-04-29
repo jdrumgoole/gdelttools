ce6aee78ef6733737ed35f4e7a27f84ce9da82cf
Judy2k <judy@judy.co.uk>
2022-04-29 11:04:09 +0100

    Add some basic docs for mongoimport.sh

--- mongoimport.sh
+++ mongoimport.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env sh
+# Upload all the CSV files in your current directory to a MongoDB cluster.
+#
+# Any parameters passed to this command are passed to mongoimport, so to
+# connect to your cluster, provide the --uri parameter with your MongoDB
+# connection string, like this:
+# 
+# ./mongoimport.sh --uri "mongodb+srv://<username>:<password>@abcde.mongodb.com/<yourdatabase>"
+
+
 cat *.export.CSV \
     | mongoimport \
         --collection=eventscsv \
