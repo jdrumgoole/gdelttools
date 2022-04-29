c229bdf507d0202aa06892356b420b2edd74d7f1
Judy2k <judy@judy.co.uk>
2022-04-29 11:30:26 +0100

    Allow mongoimport.sh to be run from a different directory.

--- mongoimport.sh
+++ mongoimport.sh
@@ -7,13 +7,39 @@
 # 
 # ./mongoimport.sh --uri "mongodb+srv://<username>:<password>@abcde.mongodb.com/<yourdatabase>"
 
+findme() {
+    # Function for finding the path of a shell script,
+    # taken from https://stackoverflow.com/a/246128
+    TARGET_FILE=$0
+
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+
+    # Iterate down a (possible) chain of symlinks
+    while [ -L "$TARGET_FILE" ]
+    do
+        TARGET_FILE=`readlink $TARGET_FILE`
+        cd `dirname $TARGET_FILE`
+        TARGET_FILE=`basename $TARGET_FILE`
+    done
+
+    # Compute the canonicalized name by finding the physical path 
+    # for the directory we're in and appending the target file.
+    PHYS_DIR=`pwd -P`
+    RESULT=$PHYS_DIR/$TARGET_FILE
+    echo $RESULT
+}
+
+myloc=$(dirname $(findme $0))
+
+fieldfile="${myloc}/gdelt_field_file.ff"
 
 cat *.export.CSV \
     | mongoimport \
         --collection=eventscsv \
         --mode=upsert \
-        --writeConcern "{w:1}" \
+        --writeConcern '{w:1}' \
         --type=tsv \
         --columnsHaveTypes \
-        --fieldFile=gdelt_field_file.ff \
+        --fieldFile="${fieldfile}" \
         $*
