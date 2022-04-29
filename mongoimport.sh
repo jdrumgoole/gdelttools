1520063b5cc641f7bca9a57e8dec48d9a0b0d96a
Judy2k <judy@judy.co.uk>
2022-04-29 11:00:53 +0100

    Pipe all the CSV files to mongoimport in one go.

--- mongoimport.sh
+++ mongoimport.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
-for i in *.export.CSV
-do
-  mongoimport --db=GDELT2 --collection=eventscsv  --type=tsv --fieldFile=gdelt_field_file.ff --mode=upsert --writeConcern "{w:1}" --columnsHaveTypes --file=$i $*
-done
-
+cat *.export.CSV \
+    | mongoimport \
+        --collection=eventscsv \
+        --mode=upsert \
+        --writeConcern "{w:1}" \
+        --type=tsv \
+        --columnsHaveTypes \
+        --fieldFile=gdelt_field_file.ff \
+        $*
